56c5b059d86e04fca7161f0e5cad540482c09cef
Andrés Felipe Chaparro <anchapar@bancolombia.com.co>
2021-08-27 11:27:34 -0500

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-card2',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mf-card2';
+}
